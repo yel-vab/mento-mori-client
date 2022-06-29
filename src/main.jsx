89f00f04eec210ee4dc8d77e6b1ed9ebd20f3447
Arielle Batasin <dev.abatasin@gmail.com>
2022-06-30 01:28:25 +0800

    updated API call and updated build files

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import MentoBookPage from './pages/MentoBookPage';
 import RecordMentoPage from './pages/RecordNewMento';
 import MentoPage from './pages/MentoPage';
 import NotFoundPage from './pages/NotFoundPage';
-import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
